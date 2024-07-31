e4edd08b9aef03ca3769619d504604d33a4e4b32
syzenzzw <162196005+syzenzzw@users.noreply.github.com>
2024-07-31 10:59:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var total = 0
+
+function comprarOleo() {
+    total = total + 20
+    alert("Você comprou um Óleo para motor Sintético")
+    document.getElementById('valortotal').innerHTML = "Total : R$ " + total
+}
+
+function show() {
+    alert("Comprou!")
+}
