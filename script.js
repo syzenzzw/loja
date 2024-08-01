9f20c15436c0139310e50c701c4347dc6c4b54e6
syzenzzw <162196005+syzenzzw@users.noreply.github.com>
2024-08-01 15:05:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function comprarOleo() {
 function show() {
     alert("Comprou!")
 }
+
+let count= 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+nextImage();
+}, 3500)
+
+function nextImage(){
+	count++;
+	if (count>6){
+		count = 1;
+
+	}
+
+	document.getElementById("radio"+count).checked = true;
+}
