863dda7ebfc967342c4127a6cb74900a323df6ac
syzenzzw <162196005+syzenzzw@users.noreply.github.com>
2024-08-03 13:40:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ var total = 0
 
 function comprarOleo() {
     total = total + 20
-    alert("Você comprou um Óleo para motor Sintético")
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
+            modal.classList.remove('abrir')
+    })
+
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
