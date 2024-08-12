c6f14f108203eea254b697ca9758e1ca277eb254
syzenzzw <162196005+syzenzzw@users.noreply.github.com>
2024-08-12 11:27:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,51 +19,35 @@ function nextSlide() {
 
 
 
+
+
+
 function comprarOleo() {
     total = total + 20
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
+   alert("Você Comprou!")
+   
 
-    document.getElementById('valortotal').innerHTML = "Total : R$ " + total
+    document.getElementById('valortotal').innerHTML = "Total: R$ " + total
 }
 
 function comprarVela() {
     total = total + 75
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
-    
+
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarFiltroOleo() {
     total = total + 35
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
     
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarFiltroAr() {
     total = total + 50
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
     
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
@@ -71,37 +55,22 @@ function comprarFiltroAr() {
 
 function comprarBobina() {
     total = total + 125
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
     
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarVelaIridium() {
     total = total + 100
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
     
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarBicoInjetor() {
     total = total + 200
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
     
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
@@ -109,61 +78,35 @@ function comprarBicoInjetor() {
 
 function comprarOleoCambio() {
     total = total + 50
-    const modal = document.getElementById('janela-modal2')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal2')
-            modal.classList.remove('abrir')
-    })
     
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarEmbreagem() {
     total = total + 150
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
+    alert("Você Comprou!")
     
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarArranque() {
     total = total + 750
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
+    alert("Você Comprou!")
     
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarConversor() {
     total = total + 1200
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
+    alert("Você Comprou!")
     
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
 function comprarCaixa() {
     total = total + 2000
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'comprar2' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
-    
+    alert("Você Comprou!")
     document.getElementById('valortotal').innerHTML = "Total : R$ " + total
 }
 
